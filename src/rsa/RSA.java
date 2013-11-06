713e6facb102ee564d1c0ebd8c1513eff73c0ae8
John Pease <johnjpease@gmail.com>
2013-11-06 17:28:33 -0600

    finished

--- src/rsa/RSA.java
+++ src/rsa/RSA.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 
 public class RSA {
 
-//	public class RSA {
 	private BigInteger p, q, e, d, n, phi_n;
 	private int size = 512;
 
@@ -18,17 +17,22 @@ public class RSA {
 		e = BigInteger.valueOf(65537);
 		SecureRandom sr = new SecureRandom();
 
+		BigInteger diff = BigInteger.valueOf(2).pow(256);
+
 		/*
 		generate 2 prime numbers greater than 512 bits and that have difference of 2^256
-		 */
+		*/
 		do {
 			sr.generateSeed(512);
-			p = new BigInteger(1024, sr);
+			p = new BigInteger(512, 256, sr);
 			sr.generateSeed(512);
-			q = new BigInteger(1024, sr);
-		} while (p.bitCount() < 512 &&
-				q.bitCount() < 512 &&
-				(p.subtract(q).compareTo(BigInteger.valueOf(2^256)) != 1));
+			q = new BigInteger(512, 256, sr);
+
+		} while (p.bitLength() < size
+				|| q.bitLength() < size
+				|| !p.isProbablePrime(256)
+				|| !q.isProbablePrime(256)
+				|| p.subtract(q).abs().compareTo(diff) != 1);
 
 		n = p.multiply(q);
 		phi_n = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
@@ -76,22 +80,31 @@ public class RSA {
 		Decryption:
 			- m = c^d (mod n)
 			- reverse the padding scheme
-	     */
+		*/
 
-	    RSA rsa = new RSA();
+		RSA rsa = new RSA();
 		int plainText;
 		BigInteger b_plainText, b_cipherText;
+		int e_start, e_end, encryptTime, d_start, d_end, decryptTime;
 
 		System.out.println("Enter any character: " );
 		plainText = System.in.read();
 		
 		b_plainText = BigInteger.valueOf(plainText);
+		e_start = (int)System.nanoTime();
 		b_cipherText = rsa.encrypt(b_plainText);
-		
+		e_end = (int)System.nanoTime();
+
 		System.out.println("plaintext:\t" + b_plainText.toString());
 		System.out.println("ciphertext:\t" + b_cipherText.toString());
-		
+
+		d_start = (int)System.nanoTime();
 		b_plainText = rsa.decrypt(b_cipherText);
+		d_end = (int)System.nanoTime();
 		System.out.println("plaintext after decryption:\t" + b_plainText.toString());
+
+		encryptTime = e_end - e_start;
+		decryptTime = d_end - d_start;
+		System.out.println("encryption time (ns): " + String.valueOf(encryptTime) + ", decryption time (ns): " + String.valueOf(decryptTime));
     }
 }
