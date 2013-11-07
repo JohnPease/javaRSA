a18df3afe6b67e0454a4baa987229fbb1d4b75f8
John Pease <johnjpease@gmail.com>
2013-11-07 10:00:26 -0600

    minor updates, removed unneeded comments

--- src/rsa/RSA.java
+++ src/rsa/RSA.java
@@ -29,10 +29,10 @@ public class RSA {
 		generate 2 prime numbers greater than 512 bits and that have difference of 2^256
 		*/
 		do {
-			sr.generateSeed(512);
-			p = new BigInteger(512, 256, sr);
-			sr.generateSeed(512);
-			q = new BigInteger(512, 256, sr);
+			sr.generateSeed(size);
+			p = new BigInteger(size, 256, sr);
+			sr.generateSeed(size);
+			q = new BigInteger(size, 256, sr);
 
 		} while (p.bitLength() < size
 				|| q.bitLength() < size
@@ -56,36 +56,12 @@ public class RSA {
 	public static void main(String[] args) throws IOException {
         /*
 	    Requirements:
-	        - BigInteger and SecureRandom should be used
-	        - p and q should be at least 512-bit
-	        - difference between p and q should be bigger than 2^256
-	        - demonstration of encryption and decryption should be given
-	        - should include encryption and decryption timing
-	        - public key e is 65537
-
-	    Key Generation:
-			1. choose 2 distinct prime numbers p and q
-			2. compute n = pq
-			3. compute phi(n) = (p-1)(q-1)
-			4. choose an integer e such that 1 < e < phi(n) and gcd(e, phi(n)) = 1
-			5. Determine d as d(inverse) = e (mod phi(n))
-
-		Public Key:
-			- consists of the modulus n and the public (encryption) exponent e
-
-		Private Key:
-			- consists of the modulus n and the private (decryption) exponent d
-
-		Notes:
-			- d, p, q, and phi(n) must be kept secret
-
-		Encryption:
-			- c = m^e (mod n)
-			- must use modulus exponentiation
-
-		Decryption:
-			- m = c^d (mod n)
-			- reverse the padding scheme
+	    - BigInteger and SecureRandom should be used
+        - p and q should be at least 512-bit
+        - difference between p and q should be bigger than 2^256
+        - demonstration of encryption and decryption should be given
+        - should include encryption and decryption timing
+        - public key e is 65537
 		*/
 
 		RSA rsa = new RSA();
